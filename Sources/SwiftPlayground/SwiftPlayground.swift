ac1d850d71a5cfdd09ca4104d62844d5a2701d13
JanneEll <janne@ellebrecht.de>
2026-04-27 22:09:08 +0000

    Apr 27, 2026, 10:09 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,15 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@main
+@main     
 struct SwiftPlayground4{
     static func main() {
         let x = 10
         let y = 5
-
         let added = x + y
         let subtracted = x - y
-
         print("\(y) % \(x) = \(x)")
 
     
