ef84a63ce715e4254ec4faa5d89751136268b74b
JanneEll <janne@ellebrecht.de>
2026-04-29 02:22:12 +0000

    Apr 29, 2026, 2:22 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,8 +6,6 @@ struct SwiftPlayground{
     static func main() {
         //playground does not work anymore, so I added this to wait till it is fixed
         print("swift works again")
-
-    
     }
 
 }
