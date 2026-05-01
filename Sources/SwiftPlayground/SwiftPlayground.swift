12133ff0e40fda04b123446accc1213c781d35b8
JanneEll <janne@ellebrecht.de>
2026-05-01 00:32:49 +0000

    May 1, 2026, 12:32 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,7 +5,7 @@
 struct SwiftPlayground {
     static func main() {
         //reminder: only have one file with "main"
-        print("ENter your name")
+        print("Enter your name")
         let userInput = readLine()!
         let age = Int(userInput)!
         let isOldEnough = age >= 18
