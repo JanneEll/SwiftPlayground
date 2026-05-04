e3be8114b026b94c976ae6ca5e55849788b7ac17
JanneEll <janne@ellebrecht.de>
2026-05-04 21:59:18 +0000

    May 4, 2026, 9:59 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,19 +5,12 @@
 struct SwiftPlayground {
     static func main() {
         //reminder: only have one file with "main"
-        let a = 6
-        let b = 7
-        let c = a + b
-        let conditionOne = c > b
-        print("C is bigger than B:", conditionOne)
-        let conditionTWO = a < b && a > 20
-        print("a is smaller than b and larger than 20:", conditionTWO)
-        let conditionThree = (b % 2) == 0 || (a % 2) != 0
-        print("b divided by two is zero and a divided by 2 is not 0:", conditionThree)
-        let conditionFour = (c % 2) != 0 || c > 50
-        print("c divided by 2 is not 0 and c is lager than 50:", conditionFour)
-        let x = conditionOne && conditionTWO && conditionThree && conditionTWO
-        print("Everything is True:", x)
-    }
+        print("Enter your score")
+        let userScore = Int(readLine()!)!
+        if userScore == 50 {
+            print("your passed the test")
+        } else {
+            print("your failed hahahahaah")
+        }
 
-}
+}}
