19fcbec9d7f996ccba48e0d534eb603ade3507a6
JanneEll <janne@ellebrecht.de>
2026-05-04 22:10:02 +0000

    May 4, 2026, 10:10 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,10 +7,30 @@ struct SwiftPlayground {
         //reminder: only have one file with "main"
         print("Enter your score")
         let userScore = Int(readLine()!)!
-        if userScore >= 50 {
-            print("your passed the test")
-        } else {
-            print("Try again in the future")
-        }
+        if userScore >= 101 {
+            print("that number is to high")
+        } else if >= 90{
+            print("Grade: A+")
+        } else if >= 85{
+            print("Grade: A")
+        } else if >= 80{
+            print("Grade: x")
+        } else if >= 75{
+            print("Grade: x")
+        } else if >= 70{
+            print("Grade: x")
+        } else if >= 65{
+            print("Grade: x")
+        } else if >= 60{
+            print("Grade: x")
+        } else if >= 55{
+            print("Grade: x")
+        } else if >= 50{
+            print("Grade: x")
+        } else if >= 45{
+            print("Grade: x")
+        } else if >= 40{
+            print("Grade: x")
+
 
 }}
