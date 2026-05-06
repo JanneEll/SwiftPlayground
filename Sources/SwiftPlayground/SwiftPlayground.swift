4cb9c9e522302700c3029ac0e586fb9aa852ac61
JanneEll <janne@ellebrecht.de>
2026-05-06 02:59:22 +0000

    May 6, 2026, 2:59 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,6 +5,7 @@
 struct SwiftPlayground {
     static func main() {
         //reminder: only have one file with "main"
+        var printed = 0
 
         // arrys
         let adultFares: [Int] = [400, 600, 750]
@@ -12,6 +13,7 @@ struct SwiftPlayground {
         let ticketTypes: [String] = ["24-Hour", "48-Hour", "72-Hour"]
 
         //asking for the data
+        
         print("What is your Age?")
         let age = Int(readLine()!)!
         print("Do you want the", ticketTypes[0], "or", ticketTypes[1], "or")
@@ -23,9 +25,16 @@ struct SwiftPlayground {
         if ticket > 0 && ticket < 4 {
             if age > 18 {
                 print("your ticket costs", adultFares[ticket - 1])
+                printed = 1
             } else {
                 print("your ticket costs", childFares[ticket - 1])
+                printed = 1
+                
             }
+        }
+        if printed == 0{
+        func main()
+        
         }
 
     }
