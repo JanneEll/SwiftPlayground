1136d02070352070f2403df629c40a2171f016cf
JanneEll <janne@ellebrecht.de>
2026-05-06 02:22:40 +0000

    May 6, 2026, 2:22 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,36 +6,7 @@ struct SwiftPlayground {
     static func main() {
         //reminder: only have one file with "main"
 
-        //enter score
-        print("Enter your score")
-        let userScore = Int(readLine()!)!
-
-        //check which grade that is
-        if userScore >= 101 {
-            print("that number is to high")
-        } else if userScore >= 90{
-            print("Grade: A+")
-        } else if userScore >= 85{
-            print("Grade: A")
-        } else if userScore >= 80{
-            print("Grade: A-")
-        } else if userScore >= 75{
-            print("Grade: B+")
-        } else if userScore >= 70{
-            print("Grade: B")
-        } else if userScore >= 65{
-            print("Grade: B-")
-        } else if userScore >= 60{
-            print("Grade: C+")
-        } else if userScore >= 55{
-            print("Grade: C")
-        } else if userScore >= 50{
-            print("Grade: C-")
-        } else if userScore >= 40{
-            print("Grade: D")
-        } else if userScore >= 0{
-            print("Grade: Failed")
-        } else {
-            print("this number is to low how tf did u score that")
-        }
+       //empty arrys
+        var adultFares [int] = []
+        var childFares [int] = []
 }}
