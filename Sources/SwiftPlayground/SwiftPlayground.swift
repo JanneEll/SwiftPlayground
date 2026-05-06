b5cc3b700e3e1ff277de662c47139eb250fbebba
JanneEll <janne@ellebrecht.de>
2026-05-06 02:48:09 +0000

    May 6, 2026, 2:48 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,31 +1,32 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@main   
+@main
 struct SwiftPlayground {
     static func main() {
         //reminder: only have one file with "main"
 
-       // arrys
-        let adultFares : [Int] = [400, 600, 750]
-        let childFares : [Int] = [800, 1200, 1500]
-        let ticketTypes : [String] = ["24-Hour", "48-Hour", "72-Hour"] 
-
+        // arrys
+        let adultFares: [Int] = [400, 600, 750]
+        let childFares: [Int] = [800, 1200, 1500]
+        let ticketTypes: [String] = ["24-Hour", "48-Hour", "72-Hour"]
 
         //asking for the data
         print("What is your Age?")
-        var Age = Int(readLine()!)!
-        print("Do you want the", ticketTypes[0], "or", ticketTypes[1], "or" )
+        var age = Int(readLine()!)!
+        print("Do you want the", ticketTypes[0], "or", ticketTypes[1], "or")
         print(ticketTypes[2], "?")
         print("Enter 1 2 or 3 deepending on which you want.")
-        var Ticket = Int(readLine()!)!
-
+        var ticket = Int(readLine()!)!
 
         //calculating the price
-        if Age > 18 {
-            print("your ticket costs", adultFares[Ticket - 1])    
-        } else {
-            print("your ticket costs", childFares[Ticket - 1])
+        if ticket > 0 && ticket < 4 {
+            if age > 18 {
+                print("your ticket costs", adultFares[ticket - 1])
+            } else {
+                print("your ticket costs", childFares[ticket - 1])
+            }
         }
 
-}}
+    }
+}
