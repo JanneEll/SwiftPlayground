df66387bf3fd1e4a41a9b179d3d2adbbb9236711
JanneEll <janne@ellebrecht.de>
2026-05-13 02:08:35 +0000

    May 13, 2026, 2:08 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,29 +5,8 @@
 struct SwiftPlayground {
     static func main() {
         //"main" is a function so i can repeat it if something went wrong
-        //checking if the user got a result yet
-        
-
-        // arrys
-        //                kid, adult, eldery
-        let Fares: [Int] = [8, 12, 10]
-        let AgeGroups: [Int] = [18, 80]
-        var input = "x"
-        var exit = 1
-        var totalPrice = 0
- 
-        //asking for the data
-        while exit == 1{
-            print("What is the age of the next user")
-            var age = Int(readLine()!)!
-            if age < 18{
-                totalPrice = totalPrice + Fares[0]
-            }
-            print("do you want to add anothe person type ")
-            print("u otherwise type q")
-            input = readLine()!.lowercased()
-            if input == "Q"{
-                exit = 0}
+    var invited = ["x"]
+    while 
 
 
 
