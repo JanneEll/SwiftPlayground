4b3a8e0f3b5ae939a78fd423f5ae0812da1a5206
JanneEll <janne@ellebrecht.de>
2026-05-13 02:47:30 +0000

    May 13, 2026, 2:47 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,9 +6,9 @@ struct SwiftPlayground {
     static func main() {
         //"main" is a function so i can repeat it if something went wrong
     var invited = ["x"]
-    var adding = 1
+    let zz = 1
     var firstRound = 0
-    while adding == 1{
+    while zz == 1{
         print("WHo do you want to add")
         var name = String(readLine()!)
         
