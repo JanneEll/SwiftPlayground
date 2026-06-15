60a0d779cad43309b71f01b9c5c60b989f7c33f5
JanneEll <janne@ellebrecht.de>
2026-06-15 22:38:00 +0000

    Jun 15, 2026, 10:38 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,38 +1,75 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-bo
 import Foundation
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
 @main
-struct swiftPLayground {
-
+struct SwiftPlayground {
     static func main() {
-        //variabels
-        var programmRun = true
-        var seenABird = false 
+        // Prints welcome message.
+        print("Welcome to Zealandia Tracker.")
 
-        //this check if the user is active and wants to start, also it creats the var "userInput" so it doesnt have to be sorted in which postion a user input will be needed first to difine
-        print("type anything to start")
-        var userInput = readLine()!
+        // Creating constants for the lists of birds and insects. Bool for the while loop. Two lists of integers that will store the number of birds and insects seen.
+        var isRunning: Bool = true
+        let birds: [String] = ["Tieke", "Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
+        let insects: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
+        var birdsSeen: [Int] = [0, 0, 0, 0, 0, 0, 0, 0]
+        var insectsSeen: [Int] = [0, 0, 0, 0, 0, 0, 0, 0]
 
+        /// Asks her if she saw a bird or insect.   
+        while isRunning {
+            print("Did you see a bird or insect/ Quit to quit: ")
+            let birdOrInsect: String = readLine()!.lowercased()
 
-        //lists
-        let birdList: [String] = [1: Tieke,2: Kākā,3: Takahē,4: Hihi,5: Kiwi,6: Pāteke,7: Tūī,8: Kererū]
+            /// Asks her to select the bird/insect from a list of known species.
+            if birdOrInsect == "bird" {
+                birds.enumerated().forEach { index, bird in
+                    print("\(index + 1). \(bird)")
+                } 
+                /// Adds to a tally of species she has seen.
+                print("Enter a number: ")
+                let birdIndex = Int(readLine()!)!  
 
-        //functions
-        //even tho the funtcions dont have to be functions it works better because the programm is easyer to read/fix
+                /// Checking if the user input is an Int and is more then 1 but less then the lenght of the array.
+                if birdIndex >= 1, birdIndex <= birds.count {
+                    print("\(birds[birdIndex - 1]) added.")
+                    birdsSeen[birdIndex - 1] += 1
+                } else {
+                    print("Enter intergers only, between 1 and \(birds.count).")
+                }
+            } else if birdOrInsect == "insect" {
+                insects.enumerated().forEach { index, insect in
+                    print("\(index + 1). \(insect)")
+                }
+                /// Adds to a tally of species she has seen.
+                print("Enter a number: ")
+                let insectIndex = Int(readLine()!)!
 
-
-        //after the user said he saw a bird this function starts
-        func addBird() {
-            print("What kind of bird have you seen:", birdList)
-            print("Type in the number of the bird")
-            userInput = readLine()!
-            //1 and 8 r the maximum number that were possible to type in(so the inbetween can is always a possible bird to chosse)
-            if userInput > 1 && userInput < 8{
-                var newBird = userInput
+                /// Checking if the user input is an Int and is more then 1 but less then the lenght of the array.
+                if insectIndex >= 1, insectIndex <= insects.count {    
+                    print("\(insects[insectIndex - 1]) added.")
+                    insectsSeen[insectIndex - 1] += 1
+                } else {
+                    print("Enter intergers only, between 1 and \(insects.count).")
+                }
+            } else if birdOrInsect == "quit" {
+                isRunning = false
+            } else {
+                print("Incorrect input. Enter bird, insect or quit")
             }
-            
         }
-        while programmRun == true{
-        addBird()
-        }
-}}
+        /// Counts the species she has seen and shows her the totals.
+        print("Today you have seen: ")
+        for (bird, birdSeen) in zip(birds, birdsSeen ) {
+            if birdSeen > 0 {
+                print("\(birdSeen): \(bird)")
+    }
+}
+        for (insect, insectSeen) in zip(insects, insectsSeen) {
+            if insectSeen > 0 {
+                print("\(insectSeen): \(insect)")
+    }
+}
+    }
+}
